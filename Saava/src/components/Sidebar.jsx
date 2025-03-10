d98ab70ba4b4161b0569932f95a16d2b3d2ce81b
SidharthP01 <105432075+SidharthP01@users.noreply.github.com>
2025-03-11 00:26:40 +0530

    new environment created and sidebar corrected

--- Saava/src/components/Sidebar.jsx
+++ Saava/src/components/Sidebar.jsx
@@ -1,9 +1,50 @@
+// import React from "react";
+// import "../styles/Sidebar.css";
+// import DashboardIcon from "@mui/icons-material/Dashboard";
+// import PersonIcon from "@mui/icons-material/Person";
+// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+// import LogoutIcon from "@mui/icons-material/Logout";
+// const Sidebar = () => {
+//   return (
+//     <div className="sidebar">
+//       <div className="top">
+//         <span className="logo">Cloud</span>
+//       </div>
+//       <hr />
+//       <div className="centre">
+//         <ul className="ull">
+//           <li>
+//             <DashboardIcon />
+//             <span>Dashboard</span>
+//           </li>
+//           <li>
+//             <PersonIcon />
+//             <span>Users</span>
+//           </li>
+//           <li>
+//             <AccountCircleIcon />
+//             <span>Profile</span>
+//           </li>
+//           <li>
+//             <LogoutIcon />
+//             <span>Logout</span>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
+
+// src/components/Sidebar.jsx
 import React from "react";
 import "../styles/Sidebar.css";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonIcon from "@mui/icons-material/Person";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -11,22 +52,22 @@ const Sidebar = () => {
         <span className="logo">Cloud</span>
       </div>
       <hr />
-      <div className="centre">
-        <ul className="ull">
+      <div className="center">
+        <ul className="menu">
           <li>
-            <DashboardIcon />
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
           <li>
-            <PersonIcon />
+            <PersonIcon className="icon" />
             <span>Users</span>
           </li>
           <li>
-            <AccountCircleIcon />
+            <AccountCircleIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
-            <LogoutIcon />
+            <LogoutIcon className="icon" />
             <span>Logout</span>
           </li>
         </ul>
