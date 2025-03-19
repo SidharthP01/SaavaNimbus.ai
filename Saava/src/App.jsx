d582e450a4683f8b90743011335b09fadb260b04
AdhidevAthman <adhidevakaathman@gmail.com>
2025-03-19 12:16:32 +0530

    dashboard

--- Saava/src/App.jsx
+++ Saava/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
